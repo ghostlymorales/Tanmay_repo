4893ae63713381bd115eebe7bb62fa673577adae
ghostlymorales <59527119+ghostlymorales@users.noreply.github.com>
2025-08-15 06:12:55 +0000

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print ("Welcome to brand name generator\n")
+
+name2 = input("please enter your city name\n")
+print ("\n")
+
+pet_name = input ('please enter your "pet" name\n')
+print("\n")
+
+print ("Your brand name is")
+print("\n")
+
+print ( name2 + " " + pet_name, "!")
